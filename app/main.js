3c5ca4a9e8f6711350a9ccad5741a9936e9d28e6
Jose Pablo Granados <jpgcodecr@gmail.com>
2016-10-14 10:12:21 -0600

    Adding smaller number exercise

--- app/main.js
+++ app/main.js
@@ -3,9 +3,11 @@ import countVowels from './modules/countVowels';
 import countConsonants from './modules/countConsonants';
 import factorial from './modules/factorial';
 import fibonacci from './modules/fibonacci';
+import smallerNumber from './modules/smallerNumber';
 
 console.log('isPalindrome: ', isPalindrome('sas'));
 console.log('Vowels: ', countVowels('Pedro222'));
 console.log('Consonants:', countConsonants('Hola'));
-console.log(factorial(4));
-console.log(fibonacci(7));
+console.log('Factorial', factorial(4));
+console.log('Fibonacci', fibonacci(7));
+console.log('SmallerNumber:', smallerNumber([24,3,22,20]));
---
+++ app/modules/smallerNumber.js
@@ -0,0 +1,23 @@
+/*
+ smallerNumber.js
+ Get the smaller number in an array
+ Author: Jose Pablo Granados <jpgcodecr@gmail.com>
+*/
+
+const smallerNumber = arr => {
+
+	const number = arr.reduce(function(prev, curr){
+		if(prev === 0){
+			return curr;
+		}else if(prev > curr){
+			return curr;
+		}else{
+			return prev;
+		}
+	}, 0);
+
+	return number;
+
+}
+
+export default smallerNumber;
